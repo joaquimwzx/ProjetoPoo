35d7d461188d0e13bfc1aef9e20c0e89be012ab7
joaquimwzx <jocavascaino68@gmail.com>
2026-04-14 17:24:53 -0300

    Add files via upload

---
+++ ProjetoPoo1/src/br/ulbra/dao/UsuarioDAO.java
@@ -0,0 +1,68 @@
+package br.ulbra.dao;
+
+import br.ulbra.model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class UsuarioDAO {
+
+    Connection con;
+
+    public UsuarioDAO() throws SQLException {
+        con = ConnectionFactory.getConnection();
+    }
+
+    public boolean checkLogin(String email, String senha) {
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;//so usado em select
+
+        boolean check = false;
+
+        try {
+
+            
+            stmt = con.prepareStatement("SELECT * FROM tbusuario WHERE email = ? and senha = ?");
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                check = true;
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+
+        return check;
+
+    }
+
+    public void create(Usuario u) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("INSERT INTO tbusuario(nome, email, senha, sexo) VALUES (?,?,?,?)");
+
+            stmt.setString(1, u.getNome());
+            stmt.setString(2, u.getEmail());
+            stmt.setString(3, u.getSenha());
+            stmt.setString(4, u.getSexo());
+
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Usuário Salvo com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro:" + ex.getMessage());
+
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+ 
+}
